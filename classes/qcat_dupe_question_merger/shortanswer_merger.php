2a1898ee3be132cad045be213a913aa26fa9e69b
Kenneth Hendricks <kennethhendricks@catalyst-au.net>
2018-02-09 13:33:43 +1100

    Fetch shortanswer questions with the same name
    - Also add stubs for other methods that will follow

--- classes/qcat_dupe_question_merger/shortanswer_merger.php
+++ classes/qcat_dupe_question_merger/shortanswer_merger.php
@@ -33,7 +33,66 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/admin/tool/objectfs/lib.php');
 
 class shortanswer_merger {
+
+    public static $qtype = 'shortanswer';
+
     public static function merge_duplicates($qcat) {
-        echo "called\n";
+        // Get all shortanswer questions with same name
+        $samenamegroups = self::get_question_groups_with_same_name($qcat);
+
+        // Group now based on base question data.
+        $samequestiongroups = array();
+        foreach ($samenamegroups as $group) {
+            $samequestiongroups[] = self::subgroup_based_on_question($group);
+        }
+
+        $identicalquestions[] = array();
+        foreach ($samequestiongroups as $group) {
+            $identicalquestions[] = self::subgroup_based_on_specific_qtype_details($group);
+        }
+
+        var_dump($identicalquestions);
+    }
+
+    private static function get_question_groups_with_same_name($qcat) {
+        global $DB;
+        $sql = "SELECT * from {question} q
+                WHERE q.category = :qcatid
+                    AND q.qtype = :qtype
+                    AND q.name in ( SELECT q.name
+                                    FROM {question} q
+                                    INNER JOIN mdl_question_categories qc on qc.id = q.category
+                                    WHERE q.category = :qcatidtwo
+                                        AND q.qtype = :qtypetwo
+                                    GROUP BY q.name
+                                    HAVING count(q.name) > 1)";
+
+        $params = array(
+            'qcatid'        => $qcat->id,
+            'qcatidtwo'     => $qcat->id,
+            'qtype'         => self::$qtype,
+            'qtypetwo'      => self::$qtype,
+        );
+
+        $questions = $DB->get_records_sql($sql, $params);
+
+        // Group by name
+        $groupedquestions = array();
+        foreach ($questions as $question) {
+            if (!isset($groupedquestions[$question->name])) {
+                $groupedquestions[$question->name] = array();
+            }
+            $groupedquestions[$question->name][] = $question;
+        }
+
+        return $groupedquestions;
+    }
+
+    private static function subgroup_based_on_question($questions) {
+        return $questions;
+    }
+
+    private static function subgroup_based_on_specific_qtype_details($questions) {
+        return $questions;
     }
 }
